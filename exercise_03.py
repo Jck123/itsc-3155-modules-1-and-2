d8ab41fad655f5ebc1bae8aac7d979eaa8232e3e
Jck123 <jck.1.2.3.a@gmail.com>
2023-01-17 17:43:05 -0500

    Initial Commit

---
+++ exercise_03.py
@@ -0,0 +1,10 @@
+#James Kelly
+
+num = int(input("Enter a number greater than 1: "))
+
+if not (num > 1):
+    print("Invalid number! Please try again...")
+    exit()
+
+for i in range(num + 1):
+    print("The cube of " + str(i) + " is " + str(i ** 3))
